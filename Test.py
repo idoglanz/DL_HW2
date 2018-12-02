df331e44e6a7b5e4e6ad440962d3cc63b7e59e8a
idoglanz <idoglanz@gmail.com>
2018-12-02 09:15:46 +0200

    Sunday Morning

--- Test.py
+++ Test.py
@@ -3,31 +3,136 @@ import numpy as np
 import pickle, gzip, urllib.request, json
 
 
-def convert_vector_to_matrix(y):
-    y = y[:, np.newaxis]
-    return y
+# -------------------------------------- Activation class -----------------------------------------
+
+class Activations:
+
+    def __init__(self):
+        self.value = 0
+        self.back = None
+
+    def sigmoid(self, z, back=False):
+        print(z)
+        sig = 1 / (1 + np.exp(-z))
+        print(sig)
+        if back is False:
+            return sig
+        else:
+            return sig * (1-sig)
+
+    def relu(self, z, back=False):
+        if back is False:
+            return np.maximum(z, 0)
+
+        else:
+            new_z = np.zeros((z.shape[0], z.shape[1]))
+            new_z[z > 0] = 1
+
+            return new_z
+
+    def softmax(self, z, back=False):
+
+        # a = np.exp(z)
+        # b = (np.exp(z).sum(axis=1, keepdims=True))
+        #
+        # temp = np.divide(a, b)
+        temp = np.exp(z) / (np.exp(z).sum(axis=1, keepdims=True))
+        if back is False:
+            return temp
+        else:
+            return temp*(np.eye(z.shape[0], z.shape[1])-temp)
+
+    def none(self,z, back=False):
+        if back is False:
+            return z
+        else:
+            return 1
 
 
-def shuffle_data(x, y):
-    m = x.shape[1]
-    print(m)
-    xy = np.concatenate((x, y), axis=1)
-    np.random.shuffle(xy)
-    print(xy)
-    return xy[:, :-1], convert_vector_to_matrix(xy[:, m])
+# ------------------------------------------ functions ------------------------------------------
 
 
-X = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
+def activate(z, func, back=False):
+        switcher = {
+                  'relu': activ.relu,
+                  'sigmoid': activ.sigmoid,
+                  'softmax': activ.softmax,
+                  'none': activ.none
+        }
+        activation_chosen = switcher.get(func, 'invalid activation function')
+        if back is False:
+            return activation_chosen(z)
+        else:
+            return activation_chosen(z, True)
+
+
+# # ############################################################################################################
+# #
+# #
+# # def find_mean(data_set):
+# #     train_mean = np.mean(data_set, axis=0)
+# #     return train_mean
+# #
+# # ############################################################################################################
+# #
+# #
+# # def preprocess(data_set, train_mean):
+# #     data_set_processed = data_set - train_mean
+# #     return data_set_processed
+# #
+# # ############################################################################################################
+# #
+# #
+# # def class_process_old(y):
+# #     y = [(1 if (y[i]%2 == 0) else 0) for i in range(len(y))]
+# #     y = np.array(y)
+# #     return y
+# #
+# #
+# # def class_process_mnist(y, classes):
+# #
+# #     y_new = np.zeros((len(y), classes))
+# #     row = np.linspace(0, y.shape[0]-1, y.shape[0], dtype='int')
+# #     col = y
+# #     y_new[row, col] = 1
+# #
+# #     return y_new
+# #
+# #
+# # ############################################################################################################
+# #
+# #
+# # def convert_vector_to_matrix(y):
+# #     y = y[:, np.newaxis]
+# #     return y
+# #
+# # # ------------------------------------------ Main ------------------------------------------
+# #
+# # # Load data-set and preprocess (normalize)
+# #
+# # # data_url = "http://deeplearning.net/data/mnist/mnist.pkl.gz"
+# # # urllib.request.urlretrieve(data_url, "mnist.pkl.gz")
+# # with gzip.open('mnist.pkl.gz', 'rb') as f:
+# #     train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+# #
+# # train_mean = find_mean(train_set[0])
+# # X_ = preprocess(train_set[0], train_mean)
+# # X_val = preprocess(valid_set[0], train_mean)
+# # X_test = preprocess(test_set[0], train_mean)
+# # y_ = convert_vector_to_matrix(class_process_mnist(train_set[1], 10))
+# # y_val = convert_vector_to_matrix(class_process_mnist(valid_set[1], 10))
+# # y_test = convert_vector_to_matrix(class_process_mnist(test_set[1], 10))
+# #
+# #
+# # print(y_[3040, :])
+# # print(train_set[1][3040])
+# #
+
+X = np.array([[1, 2, 3], [10, 3, 4], [1, 2, 3], [1, 2, 3]])
 y = np.array([[1], [2], [3], [4]])
 
-print(X.shape)
-print(y.shape)
 
-[X_new, y_new] = shuffle_data(X, y)
-print(y_new.shape)
-print('')
-print(X - X_new)
-print(y - y_new)
+activ = Activations()
+print(X)
+print(activate(X, 'sigmoid'))
 
-w = np.zeros(5, 1)
-print(w)
--- mydnn.py
+++ mydnn.py
@@ -1,18 +1,15 @@
 import numpy as np
 import matplotlib as plt
 import timeit
-import pickle
-import gzip
-import json
-import urllib.request
-from numpy import linalg as LA
+import pickle, gzip, json, urllib.request
+# from numpy import linalg as LA
 
 # ------------------------------------------ DNN Class ------------------------------------------
 
 
 class MyDNN:
 
-    def __init__(self, architecture, Loss, weight_decay = 0):
+    def __init__(self, architecture, Loss, weight_decay=0.0):
         self.weight_decay = weight_decay
         self.Loss = Loss
         self.architecture = architecture
@@ -24,7 +21,6 @@ class MyDNN:
             layer_output_dim = layer["output"]
 
             # init of weights and biases (uniform for w and zeros for biases)
-
             n = 1/np.sqrt(layer_input_dim)
             self.params['W' + str(layer_num)] = np.random.uniform(-n, n, (layer_output_dim, layer_input_dim))
             self.params['b' + str(layer_num)] = np.zeros((layer_output_dim, 1))
@@ -49,42 +45,56 @@ class MyDNN:
             # Run training for epoch:
 
             for k in range(int(iterations)):
+
                 x_batch = x_shuf[k*batch_size:((k+1)*batch_size), :]
-                y_batch = y_shuf[k*batch_size:((k+1)*batch_size)]
+                y_batch = y_shuf[k*batch_size:((k+1)*batch_size), :]
+
                 [y_tag, history] = forwardprop(x_batch, self.params, self.architecture)
 
-                [loss, accu] = calc_loss(y_batch, y_tag, self.Loss, self.params, self.architecture)
-                print(loss.shape, accu)
-            #     gradients = backwardprop(self.params, loss[k], y_batch, y_tag, history)
-            #
-            #     lr = max(learning_rate*learning_rate_decay**(k/decay_rate), min_lr)
-            #     update_weights(self.params, gradients, lr)
-            #
-            # stop = timeit.default_timer()
-            #
-            # # Save all relevant info from epoch:
-            #
-            # if x_val is not None:
-            #     [y_tag_val, history_val] = forwardprop(x_val, self.params)
-            #     [loss_val[i], accu_val[i]] = calc_loss(y_val, y_tag_val, self.Loss, self.params, self.architecture)
-            #     history['val_loss' + str(i)] = loss_val[i]
-            #     history['val_accu' + str(i)] = accu_val[i]
-            #
-            # history['weights_epoch' + str(i)] = self.params()
-            # history['loss' + str(i)] = loss
-            # history['accu' + str(i)] = accu
-            # history['runtime' + str(i)] = stop-start
-            #
-            # # Print status message after every epoch
+                [loss, accu] = calc_loss(y_batch, y_tag, self.Loss, self.params, self.architecture, self.weight_decay)
 
-            # print(['Epoch ' + str(i) + '/' + str(epochs) + ' - ' + str(stop-start) + ' Seconds - loss: '
-            #        + str(loss_train[i]) + ' - acc: ' + str(accu_train[i]) + ' - val_loss: ' + str(loss) +
-            #        ' - val_acc: ' + str(accu)])
+                gradients = backprop(self.params, self.Loss, y_batch, y_tag, history, self.architecture, self.weight_decay)
 
-        return self.params
+                lr = max(learning_rate*learning_rate_decay**(k/decay_rate), min_lr)
+                # print('Epoch, k & lr:')
+                # print(i, k, lr)
+                # print('ytag')
+                # print(y_tag)
+                # print('Old params:')
+                # print(self.params['W2'])
+                # print('gradients')
+                # print(gradients['dW1'])
+
+                self.params = update_weights(self.params, gradients, lr, self.architecture)
+
+                # print('new params:')
+                # print(self.params['W2'])
+
+            stop = timeit.default_timer()
+
+            # Save all relevant info from epoch:
+
+            if x_val is not None:
+                [y_tag_val, history_val] = forwardprop(x_val, self.params, self.architecture)
+                [loss_val, accu_val] = calc_loss(y_val, y_tag_val, self.Loss, self.params, self.architecture, self.weight_decay)
+                history['val_loss' + str(i)] = loss_val
+                history['val_accu' + str(i)] = accu_val
+
+            history['weights_epoch' + str(i)] = self.params
+            history['loss' + str(i)] = loss
+            history['accu' + str(i)] = accu
+            history['runtime' + str(i)] = stop-start
+
+            # Print status message after every epoch
+
+            print(['Epoch ' + str(i+1) + '/' + str(epochs) + ' - ' + str(stop-start) + ' Seconds - loss: '
+                   + str(loss) + ' - acc: ' + str(accu) + ' - val_loss: ' + str(loss_val) +
+                   ' - val_acc: ' + str(accu_val)])
+
+        return self.params, history
 
 
-def predict(self,X, batch_size=None):
+def predict(self, X, batch_size=None):
 
     if batch_size is not None:
         batches = X.size[0] / batch_size
@@ -97,7 +107,7 @@ def predict(self,X, batch_size=None):
     return pred
 
 
-def evaulate(self, x, y, batch_size=None):
+def evaulate(self, X, y, batch_size=None):
 
     y_bar = predict(X, batch_size)
     [loss, accu] = calc_loss(y, y_bar, self.Loss, self.params, self.architecture)
@@ -106,7 +116,7 @@ def evaulate(self, x, y, batch_size=None):
     return loss, accu
 
 
-#-------------------------------------- Activation class -----------------------------------------
+# -------------------------------------- Activation class -----------------------------------------
 
 class Activations:
 
@@ -119,21 +129,28 @@ class Activations:
         if back is False:
             return sig
         else:
-            return sig*(1-sig)
+            return sig * (1-sig)
 
     def relu(self, z, back=False):
         if back is False:
             return np.maximum(z, 0)
+
         else:
-            new_z = z[z<=0] = 0
+            new_z = np.zeros((z.shape[0], z.shape[1]))
+            new_z[z > 0] = 1
+
             return new_z
 
     def softmax(self, z, back=False):
-        temp = np.exp(z)/np.sum(np.exp(z))
+
+        exps = np.exp(z - np.max(z, axis=1, keepdims=True))
+        temp = exps / np.sum(exps, axis =1, keepdims=True)
+
+        # temp = np.exp(z) / (np.exp(z).sum(axis=1, keepdims=True))
         if back is False:
             return temp
         else:
-            return temp*(1-temp)
+            return temp*(np.eye(z.shape[0], z.shape[1])-temp)
 
     def none(self,z, back=False):
         if back is False:
@@ -142,7 +159,7 @@ class Activations:
             return 1
 
 
-#------------------------------------------ functions ------------------------------------------
+# ------------------------------------------ functions ------------------------------------------
 
 
 def activate(z, func, back=False):
@@ -163,96 +180,108 @@ def shuffle_data(x, y):
     m = x.shape[1]
     xy = np.concatenate((x, y), axis=1)
     np.random.shuffle(xy)
-    return xy[:, :-1], convert_vector_to_matrix(xy[:, m])
+    x_new = xy[:, :m]
+    y_new = xy[:, m:]
+    return x_new, y_new
 
 
 def frwdprop_layer(x, w, b, activation):
-    z = np.dot(x, w.T)+b.T
-
+    z = np.dot(x, w.T) + b.T
     return activate(z, activation), z
 
 
 def forwardprop(x, params, architecture):
     a_curr = x
-    history = {}
+    history_local = {}
 
-    for idx, layer in enumerate(architecture, 1):
-        [a_new, z_new] = frwdprop_layer(a_curr, params['W'+str(idx)], params['b'+str(idx)], params['actv'+str(idx)])
+    for idx, layer in enumerate(architecture):
+        [a_new, z_new] = frwdprop_layer(a_curr, params['W'+str(idx+1)], params['b'+str(idx+1)], params['actv'+str(idx+1)])
+
+        history_local["A" + str(idx)] = a_curr
+        history_local["z" + str(idx+1)] = z_new
+
         a_curr = a_new
-        history["A" + str(idx)] = a_new
-        history["z" + str(idx-1)] = z_new
 
-    return a_curr, history
+    return a_curr, history_local
 
 
 def backprop_layer(dA_curr, W, z_curr, A_prev, activation):
-    m = A_prev.shape[1]
+
+    m = dA_curr.shape[0]
 
     # to update W we need: dL/dA_curr(=dA_curr) * dA_curr/dsigma(=dA_curr) * dsigma/dz(=activation') * dz/dw(=A_prev)
     # to update b we need: dL/dA_curr(=dA_curr) * dA_curr/dsigma(=dA_curr) * dsigma/dz(=activation') * dz/dw(=A_prev)
 
-    dL_dz = dA_curr * activate(z_curr, activation, back)
-    dL_dw = np.dot(dL_dz, A_prev.T)/m
-    dL_db = np.sum(dL_dz, axis=1, keepdims=True) / m
-    dL_dA_prev = np.dot(W.T, dsigma_dz)    # will be needed for previous layer
+    dL_dz = dA_curr * activate(z_curr, activation, True)
+    dL_dw = (1/m)*np.dot(dL_dz.T, A_prev)
+    dL_db = np.sum(dL_dz, axis=0, keepdims=True) / m
+    dL_dA_prev = np.dot(dL_dz, W)    # will be needed for previous layer
 
-    return dL_dA_prev, dL_dw, dL_db
+    return dL_dA_prev, dL_dw, dL_db.T
 
 
-def backprop(params, loss, y_batch, y_tag, history):
+def backprop(params, loss, y_batch, y_tag, history, architecture, weight_decay):
     gradients = {}
-    m = y_batch.shape[1]
-    y = y_batch.reshape(Y_tag.shape)
-    if loss == 'CrossEntropy':
-        dL_dy = - (np.divide(y_batch, y_tag) - np.divide(1 - y_batch, 1 - y_tag))  # NEEDS TO BE VARIFIED!!
+    m = y_batch.shape[0]
+
+    if loss == 'cross_entropy':
+
+          # dL_dy = - 1/m * (y_batch * np.divide(1, y_tag))
+
+          dL_dy = y_tag
+          dL_dy[range(m), y_batch.argmax(axis=1)] -= 1
+          dL_dy = dL_dy / m
 
     elif loss == 'MSE':
-        dL_dy = - (1/m)*np.sum(np.dot(y-y_tag,(y-y_tag).T))
+        dL_dy = - (1/m)*np.sum(y-y_tag, axis=1)
 
     else:
         raise Exception('Loss function not supported')
 
     dA_prev = dL_dy  # gradient of previous state (in last layer = y)
 
-    for prev_idx, layer in reversed(list(enumerate(params))):
+    for prev_idx, layer in reversed(list(enumerate(architecture))):
         curr_layer = prev_idx + 1
         dA_curr = dA_prev
 
-        A_prev = history["A" + str(layer_idx_prev)]
-        z_curr = history["Z" + str(layer_idx_curr)]
+        A_prev = history["A" + str(prev_idx)]
+        z_curr = history["z" + str(curr_layer)]
 
         [dA_prev, dw, db] = backprop_layer(dA_curr, params['W'+str(curr_layer)], z_curr, A_prev, params['actv' + str(curr_layer)])
 
-        gradients['dW' + str(curr_layer)] = dw
+        gradients['dW' + str(curr_layer)] = dw + weight_decay * layer_weight(params['W'+str(curr_layer)], params['regularization' + str(curr_layer)])
+        # print(['dW layer ' + str(curr_layer)])
+        # print(dw)
         gradients['db' + str(curr_layer)] = db
 
-    return gradiants
+    return gradients
 
 
-def update_weights(params, gradients, lr):
-
-    for index, layer in enumerate(params):
-        params["W" + str(index)] -= lr * gradients["dW" + str(index)]
-        params["b" + str(index)] -= lr * gradients["db" + str(index)]
+def update_weights(params, gradients, lr, architecture):
+    # print('weights updated')
+    for index, layer in enumerate(architecture, 1):
+        params['W' + str(index)] -= lr * gradients["dW" + str(index)]
+        params['b' + str(index)] -= lr * gradients["db" + str(index)]
 
     return params
 
 
-def calc_loss(y, y_bar, loss_func, params, architecture):
+def calc_loss(y, y_bar, loss_func, params, architecture, weight_decay):
 
     regularization_value = weights_weight(params, architecture)
     m = y.shape[0]
 
     if loss_func is 'MSE':    # for regression
         accu = None
-        loss = -(1/m)*np.dot((y-y_bar), (y-y_bar).T) + regularization_value
+        loss = -(1/m)*np.dot((y-y_bar).T, (y-y_bar)) + weight_decay*regularization_value
 
     elif loss_func is 'cross_entropy':  # for classification
-        loss = 1 / m * (np.dot(y, np.log(y_bar).T) + np.dot(1 - y, np.log(1 - y_bar).T)) + regularization_value
-        prob = np.copy(y_bar)
-        prob[prob > 0.5] = 1
-        prob[prob <= 0.5] = 0
-        accu = (prob == y).all(axis=0).mean()
+
+        loss = -(1 / m) * np.trace(np.dot(y, np.log(y_bar).T))  # + weight_decay * regularization_value
+        prob = np.zeros((y_bar.shape[0], y_bar.shape[1]))
+        prob[:, np.argmax(y_bar, axis=1)] = 1
+
+        accu = 1/y.shape[0] * np.sum(y*prob)
 
     else:
         loss = None
@@ -261,15 +290,26 @@ def calc_loss(y, y_bar, loss_func, params, architecture):
     return loss, accu
 
 
+def layer_weight(w, regularization):
+    if regularization is 'l1':
+        return np.sign(w)
+    elif regularization is 'l2':
+        return 2*w
+    else:
+        return w
+
+
 def weights_weight(params, architecture):
 
     w_total = 0
 
-    for idx, layer in enumerate(architecture,1):
+    for idx, layer in enumerate(architecture, 1):
         if params['regularization' + str(idx)] is 'l1':
-            w_total += LA.norm(params['W' + str(idx)], 1)
+            w_total += np.sum(np.absolute(params['W' + str(idx)]))
+
         elif params['regularization' + str(idx)] is 'l2':
-            w_total += LA.norm(params['W' + str(idx)], 2)
+            w_total += np.sum(np.power(params['W' + str(idx)], 2))
+
         elif params['regularization' + str(idx)] is 'None':
             w_total = w_total
 
@@ -293,10 +333,20 @@ def preprocess(data_set, train_mean):
 ############################################################################################################
 
 
-def class_process(y):
-    y = [(1 if (y[i]%2 == 0) else -1) for i in range(len(y))]
+def class_process_old(y):
+    y = [(1 if (y[i]%2 == 0) else 0) for i in range(len(y))]
     y = np.array(y)
     return y
+
+
+def class_process_mnist(y, classes):
+
+    y_new = np.zeros((len(y), classes))
+    row = np.linspace(0, y.shape[0]-1, y.shape[0], dtype='int')
+    col = y
+    y_new[row, col] = 1
+
+    return y_new
 
 ############################################################################################################
 
@@ -315,22 +365,23 @@ with gzip.open('mnist.pkl.gz', 'rb') as f:
     train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
 
 train_mean = find_mean(train_set[0])
+
 X_ = preprocess(train_set[0], train_mean)
 X_val = preprocess(valid_set[0], train_mean)
 X_test = preprocess(test_set[0], train_mean)
-y_ = convert_vector_to_matrix(class_process(train_set[1]))
-y_val = convert_vector_to_matrix(class_process(valid_set[1]))
-y_test = convert_vector_to_matrix(class_process(test_set[1]))
 
+y_ = class_process_mnist(train_set[1], 10)
+y_val = class_process_mnist(valid_set[1], 10)
+y_test = class_process_mnist(test_set[1], 10)
 
 # construct network architecture
 
-TempDNN = [{"input": 784, "output": 1000, "nonlinear": "relu", "regularization": "l2"},
-           {"input": 1000, "output": 1000, "nonlinear": "sigmoid", "regularization": "l2"},
-           {"input": 1000, "output": 1, "nonlinear": "softmax", "regularization": "l1"}]
+TempDNN = [{"input": 784, "output": 800, "nonlinear": "sigmoid", "regularization": "l1"},
+           {"input": 800, "output": 300, "nonlinear": "relu", "regularization": "l1"},
+           {"input": 300, "output": 10, "nonlinear": "softmax", "regularization": "l1"}]
 
-Loss = 'MSE'
-weight_decay = 0.1
+Loss = 'cross_entropy'
+weight_decay = 0.01
 
 # init DNN and activations
 
@@ -338,8 +389,7 @@ activ = Activations()
 
 DNN = MyDNN(TempDNN, Loss, weight_decay)
 
-# print(DNN.params['W' + str(1)].shape)
-# print(DNN.params['b' + str(1)].shape)
-weights = DNN.fit(X_, y_, 1, 1000, 0.4, 1, 1, 0, X_val, y_val)
+
+[weights, history1] = DNN.fit(X_, y_, epochs=10, batch_size=5000, learning_rate=0.5, learning_rate_decay=1, decay_rate=1, min_lr=0.0, x_val=X_val, y_val=y_val)
 
 
